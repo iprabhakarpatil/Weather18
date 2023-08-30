ba5620c859a5910dd06027da1c64fa86fa88bae2
Prabhakar Patil <prabhakarpatil648@gmail.com>
2023-08-30 13:34:51 +0530

    added required model files for location and weather data. added servicable protocol for connecting to webservice

---
+++ Weather18/Models/LocationGeocode.swift
@@ -0,0 +1,15 @@
+//
+//  LocationGeocode.swift
+//  Weather18
+//
+//  Created by Prabhakar Patil on 30/08/23.
+//
+
+import Foundation
+
+struct LocationGeocode: Codable {
+    
+    let name : String?
+    let lat : Double?
+    let lon : Double?
+}
---
+++ Weather18/Networkings/WebServicable.swift
@@ -0,0 +1,31 @@
+//
+//  WebServicable.swift
+//  Weather18
+//
+//  Created by Prabhakar Patil on 30/08/23.
+//
+
+import Foundation
+
+public enum WebServicableError: Error {
+    
+    case invalidStatusCode
+    case invalidURL(String)
+    case invalidPayload(URL)
+    case forwarded(Error)
+}
+
+public protocol WebServicable {
+    
+    var apikey: String { get }
+    
+    func fetchGeocode(for city: String) async throws -> (latitude: String?, longitude: String?)
+    func fetchWeatherData(for geocodes: (latitude: Double, longitude: Double)) async throws -> (String?, WebServicableError)
+}
+
+extension WebServicable {
+    
+    var apikey: String {
+        return "e1018664c2956d39c59bcb0ff03ece46"
+    }
+}
