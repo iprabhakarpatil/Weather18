90df12d4ea9ff368db5c4dce789e7792079e75e3
Prabhakar Patil <prabhakarpatil648@gmail.com>
2023-08-30 13:21:38 +0530

    updated home view. request for location permission. locationmanager added.

---
+++ Weather18/LocationManagers/LocationManagerObservable.swift
@@ -0,0 +1,50 @@
+//
+//  LocationManagerObservable.swift
+//  Weather18
+//
+//  Created by Prabhakar Patil on 30/08/23.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManagerObservable: NSObject, ObservableObject {
+    
+    private let locationManager = CLLocationManager()
+
+    @Published private (set) var locationStatus: CLAuthorizationStatus?
+    @Published private (set) var currentLocation: CLLocation?
+    
+    override init() {
+        
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest /// set the location accuracy to best for fetching desired results.
+        locationManager.requestWhenInUseAuthorization() /// only authorised to use when weather app is open.
+        locationManager.startUpdatingLocation() /// start the location services.
+    }
+    
+}
+
+
+// MARK: - CLLocationManagerDelegate
+extension LocationManagerObservable: CLLocationManagerDelegate {
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        
+        locationStatus = manager.authorizationStatus
+        print("Current authorization status: \(locationStatus ?? .denied)")
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        guard let latestLocation = locations.last else {
+            return
+        }
+        print("Latest user location: \(latestLocation)")
+        
+        currentLocation = latestLocation
+    }
+    
+}
--- Weather18/Views/HomeView.swift
+++ Weather18/Views/HomeView.swift
@@ -8,16 +8,33 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @StateObject private var locationManager =  LocationManagerObservable()
+    
+    private var latitude: String? {
+        return "\(locationManager.currentLocation?.coordinate.latitude)"
+    }
+    
+    private var longitude: String? {
+        return "\(locationManager.currentLocation?.coordinate.longitude)"
+    }
+    
+    
     var body: some View {
-        Text("Home Location")
         
-            .tabItem {
-                Label("Current", systemImage: "location")
-            }
-            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity )
-            .background(.linearGradient(colors: [.teal, .teal.opacity(0.5), .teal.opacity(0.2)],
-                                        startPoint: .top,
-                                        endPoint: .bottom))
+        VStack(alignment: .leading){
+            
+            
+            
+        }
+        
+        .tabItem {
+            Label("Current", systemImage: "location")
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity )
+        .background(.linearGradient(colors: [.teal, .teal.opacity(0.5), .teal.opacity(0.2)],
+                                    startPoint: .top,
+                                    endPoint: .bottom))
     }
 }
 
