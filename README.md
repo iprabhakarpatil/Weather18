1c8c999ceaaf1dd68e40b5ed00ae3bd6b058db14
Prabhakar Patil <prabhakarpatil648@gmail.com>
2023-08-30 12:39:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Weather18
+ Use Open Weather Map API to fetch the weather details of the user's current location and also for the custom input locations.
---
+++ Weather18/Weather18App.swift
@@ -0,0 +1,17 @@
+//
+//  Weather18App.swift
+//  Weather18
+//
+//  Created by Prabhakar Patil on 30/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
